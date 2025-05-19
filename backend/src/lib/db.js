8de1ad51af4cb2f0a1b5ce5fde6364a91afbed80
Rohit KUmar <developerrohit7654@gmail.com>
2025-05-19 10:25:39 +0530

    Initial commit

---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+export const connectDB = async () => {
+    try {
+        const MONGODB_URI = process.env.MONGODB_URI;
+        await mongoose.connect(MONGODB_URI);
+        console.log('✅ MongoDB connected');
+    } catch (err) {
+        console.error('❌ MongoDB connection failed:', err.message);
+        process.exit(1);
+    }
+};
